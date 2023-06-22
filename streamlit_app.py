552ce94e4ca4c9cd4d3955ac54fd6c176bdff844
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-06-22 09:43:36 -0500

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,21 +12,22 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 df = pd.read_csv("usa.csv")
 dfw = pd.read_csv("world.csv")
 
-st.write(df.head(1))
 
 #Introduction : Agile Antelopes
-st.header("Agile Antelopes")
+st.title("Agile Antelopes")
 
 #Kai
-
+st.write("My name is Kai")
 #Ava
-
+st.write("I'm Ava, ")
 #Tyler
-
+st.write("Hi, I'm Tyler.")
 #Sienna
 
 #On Economic Freedom Index : Kai
-
+st.title("Economic Freedom Index")
+st.header("Introduction")
+st.write("The Economic Freedom Index is a measure that assesses the level of economic freedom and openness in a country. It takes into account factors such as the rule of law, property rights, government intervention, and trade freedom, providing a comparative analysis of economic environments worldwide.")
 # Kai
 # What are countries with most and least economic freedom ?
 
@@ -57,6 +58,9 @@ st.header("Agile Antelopes")
 
 #Ava
 # What are US states with most and least economic freedom ?
+st.title("US States with Most and Least Economic Fredom")
+st.header("Which States Have The Most Economic Freedom?")
+
 
 # Bar chart of index and top countries
 
