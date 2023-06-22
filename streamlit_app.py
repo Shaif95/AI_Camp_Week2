b1f97124e1aab855ed862161e6bfb54d46b075e0
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-06-22 08:40:05 -0500

    A

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+import io
+
+warnings.filterwarnings("ignore")
+st.set_option('deprecation.showPyplotGlobalUse', False)
