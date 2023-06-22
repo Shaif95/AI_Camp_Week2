507e2317d70c65f0db272301afd17b1c7486f6e3
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-06-22 09:05:47 -0500

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,65 @@ import io
 
 warnings.filterwarnings("ignore")
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title("Streamlit on Replit")
+
+df = pd.read_csv("usa.csv")
+dfw = pd.read_csv("world.csv")
+
+st.write(df.head(1))
+
+#Introduction : Agile Antelopes
+
+#Kai
+
+#Ava
+
+#Tyler
+
+#Sienna
+
+#On Economic Freedom Index : Kai
+
+# Kai
+# What are countries with most and least economic freedom ?
+
+# Bar chart of index and top countries
+
+#Does location affect the economic freedom summary index for countries ?
+
+#Scatter Plots for index vs region
+
+#Does economic freedom Quartile relate to region ?
+#Pie Charts
+
+#Tyler
+#How do Taxes relate to Economic Freedom Summary Index ?
+
+#Scatter Plot of Taxes vs Index (Hue Quartile)
+
+#What is relation of taxes and Govt emloyment with Quartile.
+
+#Bar chart of mean of taxes for each quartile.
+#Bar chart of mean of Govt employment with quartile.
+
+#Sienna
+
+#What is the genral distribution of Economic Freedom Index?
+
+#Histogram Plot of Economic Freedom Index
+
+#Ava
+# What are US states with most and least economic freedom ?
+
+# Bar chart of index and top countries
+
+# What is relation of Min Wage with Quartile?
+
+# Bar chart of taxes for each quartile.
+
+#Relation of Income claasification and quartile in the world ?
+
+#Pie plot for quartiles and income class
+
+#Conclusion : Ava
