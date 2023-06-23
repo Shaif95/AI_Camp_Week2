de98a058a48270fb3ad2afcd5737b793e9331dd4
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-06-23 09:10:57 -0500

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -325,5 +325,8 @@ for q in quartiles:
            names='World Bank Current Income Classification, 1990-present ',
            title=f'Income Classification in Quartile {q}'))
 
+st.write("We can see that High income countries are over represented in 1st Quartile and low Income are in 4th Quartile.")
+
+st.header("Conclusion :")
 #Conclusion : Ava
 st.write("In conclusion, Our data science project analyzed the Economic Freedom Index to gain insights into economic environments worldwide. The project team explored factors such as location, taxes, and minimum wage, providing valuable findings for understanding the relationship  between economic freedom and various influential factors.")
